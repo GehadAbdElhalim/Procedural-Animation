6b0fa6de94f849ce645ee036effb34ffa13d9298
GehadAbdElhalim <halimgego@gmail.com>
2023-11-28 01:11:16 +0200

    First test for the IK Solver

--- Assets/Main Project/Scripts/IKSolver.cs
+++ Assets/Main Project/Scripts/IKSolver.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 namespace ProceduralAnimation
 {
@@ -9,6 +11,7 @@ namespace ProceduralAnimation
         [SerializeField] List<Transform> pointTransforms = new List<Transform>();
         [SerializeField] Transform targetTransform;
         [SerializeField] int iterationsCount = 10;
+        [SerializeField] float toleranceDistance = 1f;
 
         private List<float> armLengths = new List<float>();
 
@@ -23,27 +26,19 @@ namespace ProceduralAnimation
 
         private void FixedUpdate()
         {
-            if (IsTargetBeyondReach())
-                return;
+            //if (IsTargetBeyondReach())
+            //    return;
 
             List<Vector3> newPoints = pointTransforms.Select(p => p.position).ToList();
             for (int i = 0; i < iterationsCount; i++)
             {
-                newPoints = BackwardCheck(newPoints);
-                newPoints = ForwardCheck(newPoints);
+                newPoints = BackwardPass(newPoints);
+                newPoints = ForwardPass(newPoints);
+
+                if (IsArmNearTarget()) break;
             }
-
-            //Lerp all original points to their new points
-        }
-
-        private List<Vector3> ForwardCheck(List<Vector3> points)
-        {
-            return new List<Vector3>();
-        }
-
-        private List<Vector3> BackwardCheck(List<Vector3> points)
-        {
-            return new List<Vector3>();
+            DebugLines(newPoints);
+            RotateArmPartsToMatchPoints(newPoints);
         }
 
         private bool IsTargetBeyondReach()
@@ -55,6 +50,59 @@ namespace ProceduralAnimation
             }
             float distanceFromStartPointToTarget = Vector3.Distance(targetTransform.position, pointTransforms[0].position);
             return totalArmLength < distanceFromStartPointToTarget;
+        }
+
+        private List<Vector3> ForwardPass(List<Vector3> points)
+        {
+            points[0] = pointTransforms[0].position;
+
+            for (int i = 1; i < armLengths.Count; i++)
+            {
+                Vector3 direction = (points[i] - points[i - 1]).normalized;
+                points[i] = points[i - 1] + direction * armLengths[i];
+            }
+
+            return points;
+        }
+
+        private List<Vector3> BackwardPass(List<Vector3> points)
+        {
+            points[points.Count - 1] = targetTransform.position;
+
+            for (int i = armLengths.Count - 1; i >= 0; i--)
+            {
+                Vector3 direction = (points[i] - points[i + 1]).normalized;
+                points[i] = points[i + 1] + direction * armLengths[i];
+            }
+
+            return points;
+        }
+
+        private bool IsArmNearTarget()
+        {
+            return Vector3.Distance(pointTransforms[pointTransforms.Count - 1].position, targetTransform.position) <= toleranceDistance;
+        }
+
+        private void DebugLines(List<Vector3> newPoints)
+        {
+            for (int i = 0; i < armLengths.Count; i++)
+            {
+                Debug.DrawLine(newPoints[i], newPoints[i + 1], GetRandomColor(i));
+            }
+
+            Color GetRandomColor(int seed)
+            {
+                Color[] color = new Color[] { Color.red, Color.green, Color.blue };
+                return color[seed % color.Length];
+            }
+        }
+
+        private void RotateArmPartsToMatchPoints(List<Vector3> newPoints)
+        {
+            for (int i = 0; i < armLengths.Count; i++)
+            {
+                pointTransforms[i].LookAt(newPoints[i + 1]);
+            }
         }
     }
 }
