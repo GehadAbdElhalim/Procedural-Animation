4752109d14f95cbf7cc4472672aac894f55c7815
GehadAbdElhalim <halimgego@gmail.com>
2023-12-02 23:54:21 +0200

    Made some enhancements

--- Assets/Main Project/Scripts/ArmMovement.cs
+++ Assets/Main Project/Scripts/ArmMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,30 +7,46 @@ namespace ProceduralAnimation
 {
     public class ArmMovement : MonoBehaviour
     {
+        [SerializeField] Movement body;
+
         [SerializeField] Transform raycastStartTransform;
         [SerializeField] LayerMask raycastLayers;
         [SerializeField] float maxDistanceAllowedBetweenArmAndRayCast = 5f;
 
+        public bool IsGrounded { get; private set; }
+        public bool canMove = true;
+
+        public Action OnArmStartedMoving;
+        public Action OnArmStoppedMoving;
+
         private void FixedUpdate()
         {
+            if (!canMove) return;
+
             if (Physics.Raycast(raycastStartTransform.position, raycastStartTransform.forward, out RaycastHit hitInfo, 1000f, raycastLayers))
             {
                 Debug.DrawLine(raycastStartTransform.position, hitInfo.point, Color.red);
                 if (Vector3.Distance(transform.position, hitInfo.point) > maxDistanceAllowedBetweenArmAndRayCast)
                 {
                     StopAllCoroutines();
-                    StartCoroutine(LerpToPosition(hitInfo.point, 1f));
+                    StartCoroutine(MoveArmToPosition(hitInfo.point, 0.5f));
                 }
             }
         }
 
-        IEnumerator LerpToPosition(Vector3 targetPosition, float duration)
+        IEnumerator MoveArmToPosition(Vector3 targetPosition, float duration)
         {
+            OnArmStartedMoving?.Invoke();
             float elapsedTime = 0f;
             while (elapsedTime < duration)
             {
                 elapsedTime += Time.fixedDeltaTime;
                 transform.position = Vector3.Lerp(transform.position, targetPosition, elapsedTime / duration);
+                //transform.position += body.transform.up * Mathf.Lerp(0, 2f, duration - elapsedTime / duration);
+
+                if (Vector3.Distance(transform.position, targetPosition) <= 0.5f)
+                    OnArmStoppedMoving?.Invoke();
+
                 yield return new WaitForFixedUpdate();
             }
         }
--- Assets/Main Project/Scripts/Movement.cs
+++ Assets/Main Project/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,49 @@ namespace ProceduralAnimation
 {
     public class Movement : MonoBehaviour
     {
+        [SerializeField] List<ArmMovement> armMovements1;
+        [SerializeField] List<ArmMovement> armMovements2;
+
+        private void Awake()
+        {
+            foreach (var armMovement in armMovements1)
+            {
+                armMovement.OnArmStartedMoving += () => ToggleMovementCapabiltiy(armMovements2, false);
+                armMovement.OnArmStoppedMoving += () => ToggleMovementCapabiltiy(armMovements2, true);
+            }
+
+            foreach (var armMovement in armMovements2)
+            {
+                armMovement.OnArmStartedMoving += () => ToggleMovementCapabiltiy(armMovements1, false);
+                armMovement.OnArmStoppedMoving += () => ToggleMovementCapabiltiy(armMovements1, true);
+            }
+        }
+
         private void Update()
         {
-            transform.Translate(Input.GetAxis("Vertical") * transform.forward * 5f * Time.deltaTime);
+            float xInput = Input.GetAxis("Horizontal");
+            float zInput = Input.GetAxis("Vertical");
+            float yInput = Input.GetKey(KeyCode.Space) ? 1f : 0f;
+            yInput += Input.GetKey(KeyCode.C) ? -1f : 0;
+            transform.Translate(zInput * transform.forward * 5f * Time.deltaTime, Space.World);
+            transform.Translate(yInput * transform.up * 5f * Time.deltaTime, Space.World);
+            transform.Rotate(0, xInput * 50 * Time.deltaTime, 0);
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 v1 = (armMovements1[0].transform.position - armMovements1[1].transform.position).normalized;
+            Vector3 v2 = (armMovements2[0].transform.position - armMovements2[1].transform.position).normalized;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, Vector3.Cross(v1, v2));
+        }
+
+        private void ToggleMovementCapabiltiy(List<ArmMovement> armMovements, bool IsCapableOfMoving)
+        {
+            foreach(var armMovement in armMovements) 
+            {
+                armMovement.canMove = IsCapableOfMoving;
+            }
         }
     }
 }
